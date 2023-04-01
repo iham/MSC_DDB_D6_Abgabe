5fb51ef0702d3d2539b74df51e7276930145f6f0
Markus Hilbert <markus.hilbert@iham.at>
2023-04-01 12:50:28 +0000

    clean up.

--- receipt/src/App.js
+++ receipt/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5 mb-5'>
+      <h1>Receipt Collector</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas aliquam ipsum error beatae repudiandae, modi molestiae consequuntur, sint similique quae quisquam adipisci ullam id. Laborum repudiandae totam excepturi! Blanditiis, dicta.
+      </p>
     </div>
   );
 }
--- receipt/src/App.test.js
+++ receipt/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders receipt', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/receipt/i);
+  expect(textElement).toBeInTheDocument();
 });
