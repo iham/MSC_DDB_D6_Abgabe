1eca88dc2398d4917053bb597523188238a20f79
Markus Hilbert <markus.hilbert@iham.at>
2023-04-03 08:30:39 +0000

    implemented Receipt

--- receipt/src/App.js
+++ receipt/src/App.js
@@ -1,8 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import ReceiptList from './component/ReceiptList';
 
+import Receipt from './data/Receipt';
+
 const App = props => {
   const receipts = ['r1', 'r2', 'r3', 'r3asdfas'];
+  let r0 = new Receipt();
+  let r1 = new Receipt(
+    new Date(),
+    "description",
+    "project",
+    100,
+    20,
+    "comment"
+  );
+  console.log(r0);
+  debugger;
   return (
     <div className='container mt-5 mb-5'>
       <h1>Receipt Collector</h1>
--- receipt/src/data/Receipt.js
+++ receipt/src/data/Receipt.js
@@ -1,4 +1,69 @@
-// 
 class Receipt {
-    // 
+    #receiptDate;
+    #description;
+    #project;
+    #netVal;
+    #ust;
+    #comment;
+
+    constructor(
+        receiptDate=null,
+        description="",
+        project="",
+        netVal=0,
+        ust=0,
+        comment=""
+    ) {
+        this.receiptDate = receiptDate;
+        this.description = description;
+        this.project = project;
+        this.netVal = netVal;
+        this.ust = ust;
+        this.comment = comment;
+    }
+
+    // getters 
+    get receiptDate() {
+        return this.#receiptDate;
+    }
+    get description() {
+        return this.#description;
+    }
+    get project() {
+        return this.#project;
+    }
+    get netVal() {
+        return this.#netVal;
+    }
+    get ust() {
+        return this.#ust;
+    }
+    get grossVal() {
+        return this.netVal + (this.netVal/100 * this.ust);
+    }
+    get comment() {
+        return this.#comment;
+    }
+
+    // setters
+    set receiptDate(receiptDate) {
+        this.#receiptDate = receiptDate;
+    }
+    set description(description) {
+        this.#description = description;
+    }
+    set project(project) {
+        this.#project = project;
+    }
+    set netVal(netVal) {
+        this.#netVal = netVal;
+    }
+    set ust(ust) {
+        this.#ust = ust;
+    }
+    set comment(comment) {
+        this.#comment = comment;
+    }
 }
+
+export default Receipt;
